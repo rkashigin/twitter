27ec7e21fe7d298a6b00cc0ee0eeab892164d963
Roman Kashigin <r.kashigin@yandex.ru>
2021-03-17 11:45:09 +0300

    MaterialUI theme created, MaterialUI component connected and tested

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Button>Hello</Button>
     </div>
   );
 }
